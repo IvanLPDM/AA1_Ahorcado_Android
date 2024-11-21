0cd2fcb4f401c8bedb46e096f0e326bfd3760a82
Ivan <ivan.leris@enti.cat>
2024-11-21 13:24:39 +0100

    GamePlay funcional + win Lose

--- app/src/main/java/com/example/ahorcadogame/GamePlayActivity.kt
+++ app/src/main/java/com/example/ahorcadogame/GamePlayActivity.kt
@@ -1,31 +1,54 @@
 package com.example.ahorcadogame
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.view.View
 import android.graphics.Color
 import android.widget.TextView
-
+import android.widget.ImageView
+import android.util.Log
 
 class GamePlayActivity : AppCompatActivity() {
 
-    val word = "Navidad"  // La palabra a adivinar
+    val word = "Navidad"  // La palabra a adivinar (tiene que ser un parametro que reciba)
     var letters = 0  // Contador de letras acertadas
+    var misses = 0  // Contador de letras acertadas
+    var allowedErrors = 6; //esto tiene que depender de la dificultad
+    var endGame = false
     val numLetters = word.length
     val acertadas = CharArray(numLetters) { '_' }  // Array para mostrar las letras acertadas
+    val numlettersToWin = numLetters - 1;
 
+    //Texto a adivinar "___"
+    //lateint es para que se inicialice despues de que se cree el layout
     lateinit var wordTextView: TextView
+
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game_play)
+
+        // Encuentra el diseño raíz
+        val GamePlayLayout: View = findViewById(R.id.gamePlayLayout)
+
+
+        //Imager Win
+        val imageWin = findViewById<ImageView>(R.id.WinMessage)
+        val imageLose = findViewById<ImageView>(R.id.LoseMessage)
+
+        imageWin.visibility = View.INVISIBLE
+        imageLose.visibility = View.INVISIBLE
 
         wordTextView = findViewById(R.id.wordTextView)
 
         // Inicializar la palabra con "_"
         for (i in acertadas.indices) {
             acertadas[i] = '_'
+
         }
 
         wordTextView.text = acertadas.joinToString(" ")
@@ -63,16 +86,38 @@ class GamePlayActivity : AppCompatActivity() {
         )
 
         //
+
         val buttonClickListener = View.OnClickListener { view ->
             if (view is Button) {
 
-                val letter = view.text.toString()
+                if (endGame) {
+                    // Si el juego ha terminado, no se hacen más acciones
+                    return@OnClickListener
+                }
+                    val letter = view.text.toString()
 
-                processGuess(letter)
+                    processGuess(letter)
 
-                wordTextView.text = acertadas.joinToString(" ")
+                    wordTextView.text = acertadas.joinToString(" ")
 
-                view.setBackgroundColor(Color.parseColor("#808080"))
+                    view.setBackgroundColor(Color.parseColor("#808080"))
+                    view.isEnabled = false
+
+                    // Verificar si el jugador ha ganado
+                    if (letters >= numlettersToWin) {
+
+                        imageWin.visibility = View.VISIBLE
+                        endGame = true
+                    }
+                    if(misses >= allowedErrors)
+                    {
+                        imageLose.visibility = View.VISIBLE
+                        endGame = true
+                    }
+
+
+
+
             }
 
         }
@@ -82,11 +127,16 @@ class GamePlayActivity : AppCompatActivity() {
             button.setOnClickListener(buttonClickListener)
         }
 
-        // Verificar si el jugador ha ganado o no
-        if (letters == numLetters) {
-
-            
+        //Pulsar en cualquier lado de la pantalla para cambiar a main
+        GamePlayLayout.setOnClickListener {
+            if (endGame) {
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
         }
+
+
 
     }
 
@@ -96,9 +146,13 @@ class GamePlayActivity : AppCompatActivity() {
         for (i in word.indices) {
             if (word[i].toString().equals(letter, ignoreCase = true)) {
                 acertadas[i] = word[i]
-                letters++
+                letters ++
                 correctGuess = true
             }
+        }
+        if(!correctGuess)
+        {
+            misses++
         }
     }
 }
