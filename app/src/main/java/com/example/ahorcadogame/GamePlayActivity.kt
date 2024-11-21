0ff7f16410ecb9728e86ade8cc221872d1feac1b
Ivan <ivan.leris@enti.cat>
2024-11-21 12:08:50 +0100

    Completar el texto

--- app/src/main/java/com/example/ahorcadogame/GamePlayActivity.kt
+++ app/src/main/java/com/example/ahorcadogame/GamePlayActivity.kt
@@ -5,11 +5,31 @@ import android.os.Bundle
 import android.widget.Button
 import android.view.View
 import android.graphics.Color
+import android.widget.TextView
+
 
 class GamePlayActivity : AppCompatActivity() {
+
+    val word = "Navidad"  // La palabra a adivinar
+    var letters = 0  // Contador de letras acertadas
+    val numLetters = word.length
+    val acertadas = CharArray(numLetters) { '_' }  // Array para mostrar las letras acertadas
+
+    lateinit var wordTextView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game_play)
+
+        wordTextView = findViewById(R.id.wordTextView)
+
+        // Inicializar la palabra con "_"
+        for (i in acertadas.indices) {
+            acertadas[i] = '_'
+        }
+
+        wordTextView.text = acertadas.joinToString(" ")
+
 
         // Lista de botones a los que les queremos asignar el mismo Listener
         val buttons = arrayOf(
@@ -44,10 +64,17 @@ class GamePlayActivity : AppCompatActivity() {
 
         //
         val buttonClickListener = View.OnClickListener { view ->
-
             if (view is Button) {
+
+                val letter = view.text.toString()
+
+                processGuess(letter)
+
+                wordTextView.text = acertadas.joinToString(" ")
+
                 view.setBackgroundColor(Color.parseColor("#808080"))
             }
+
         }
 
         // Asignar el mismo OnClickListener a todos los botones en la lista
@@ -55,5 +82,23 @@ class GamePlayActivity : AppCompatActivity() {
             button.setOnClickListener(buttonClickListener)
         }
 
+        // Verificar si el jugador ha ganado o no
+        if (letters == numLetters) {
+
+            
+        }
+
+    }
+
+    private fun processGuess(letter: String) {
+        var correctGuess = false
+
+        for (i in word.indices) {
+            if (word[i].toString().equals(letter, ignoreCase = true)) {
+                acertadas[i] = word[i]
+                letters++
+                correctGuess = true
+            }
+        }
     }
 }
